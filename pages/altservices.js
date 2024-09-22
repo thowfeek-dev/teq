c7b7c6f952df93eacdf2321adb27d26c62625c2d
thowfeek salim <thowfeek.s@pillsbee.com>
2024-09-22 20:24:12 +0530

    update altservices

--- pages/altservices.js
+++ pages/altservices.js
@@ -4,81 +4,73 @@
 
 <div class="container" data-aos="fade-up" data-aos-delay="100">
 
-<div class="row gy-4">
+  <div class="row gy-4">
 
-  <div class="col-lg-6" data-aos="zoom-in" data-aos-delay="200">
-    <div class="service-item position-relative">
-      <div class="img">
-        <img src="https://cdn.pixabay.com/photo/2018/08/10/05/45/computer-3596169_1280.jpg" class="img-fluid" alt="">
+    <div class="col-lg-6" data-aos="zoom-in" data-aos-delay="200">
+      <div class="service-item position-relative">
+        <div class="img">
+          <img src="https://cdn.pixabay.com/photo/2018/08/10/05/45/computer-3596169_1280.jpg" class="img-fluid" alt="">
+        </div>
+        <div class="details">
+          <a href="service-details.html" class="stretched-link">
+            <h3>WI-FI NETWORKING</h3>
+          </a>
+          <p>
+            Design and install high-performance Wi-Fi networks with optimal coverage and enterprise-grade solutions.
+          </p>
+        </div>
       </div>
-      <div class="details">
-        <a href="service-details.html" class="stretched-link">
-          <h3>WI-FI NETWORKING</h3>
-        </a>
-        <p>
-          • Design and installation of high-performance Wi-Fi networks.<br>
-          • Wi-Fi coverage optimization for maximum performance.<br>
-          • Enterprise-grade Wi-Fi solutions for businesses and homes.
-        </p>
-      </div>
-    </div>
-  </div><!-- End Service Item -->
+    </div><!-- End Service Item -->
 
-  <div class="col-lg-6" data-aos="zoom-in" data-aos-delay="300">
-    <div class="service-item position-relative">
-      <div class="img">
-        <img src="https://butterflymx.com/wp-content/uploads/2020/09/smart-intercom.jpg" class="img-fluid" alt="">
+    <div class="col-lg-6" data-aos="zoom-in" data-aos-delay="300">
+      <div class="service-item position-relative">
+        <div class="img">
+          <img src="https://butterflymx.com/wp-content/uploads/2020/09/smart-intercom.jpg" class="img-fluid" alt="">
+        </div>
+        <div class="details">
+          <a href="service-details.html" class="stretched-link">
+            <h3>SMART INTERCOM</h3>
+          </a>
+          <p>
+            Install smart intercoms for security, integrate with home automation, and offer video solutions.
+          </p>
+        </div>
       </div>
-      <div class="details">
-        <a href="service-details.html" class="stretched-link">
-          <h3>SMART INTERCOM SYSTEMS</h3>
-        </a>
-        <p>
-          • Installation of smart intercom systems for security and convenience.<br>
-          • Integration with home automation systems.<br>
-          • Video intercom solutions for enhanced communication.
-        </p>
-      </div>
-    </div>
-  </div><!-- End Service Item -->
+    </div><!-- End Service Item -->
 
-  <div class="col-lg-6" data-aos="zoom-in" data-aos-delay="400">
-    <div class="service-item position-relative">
-      <div class="img">
-        <img src="https://cdn.pixabay.com/photo/2017/06/18/23/10/cctv-2417559_1280.jpg" class="img-fluid" alt="">
+    <div class="col-lg-6" data-aos="zoom-in" data-aos-delay="400">
+      <div class="service-item position-relative">
+        <div class="img">
+          <img src="https://cdn.pixabay.com/photo/2017/06/18/23/10/cctv-2417559_1280.jpg" class="img-fluid" alt="">
+        </div>
+        <div class="details">
+          <a href="service-details.html" class="stretched-link">
+            <h3>CCTV INSTALLATION</h3>
+          </a>
+          <p>
+            Custom CCTV design and installation with HD cameras and remote monitoring for security and safety.
+          </p>
+        </div>
       </div>
-      <div class="details">
-        <a href="service-details.html" class="stretched-link">
-          <h3>CCTV INSTALLATION</h3>
-        </a>
-        <p>
-          • Custom design and installation of CCTV surveillance systems.<br>
-          • High-definition cameras and recording equipment.<br>
-          • Remote monitoring and access capabilities.
-        </p>
-      </div>
-    </div>
-  </div><!-- End Service Item -->
+    </div><!-- End Service Item -->
 
-  <div class="col-lg-6" data-aos="zoom-in" data-aos-delay="500">
-    <div class="service-item position-relative">
-      <div class="img">
-        <img src="https://cdn.pixabay.com/photo/2020/03/05/17/36/smart-home-4905026_1280.jpg" class="img-fluid" alt="">
+    <div class="col-lg-6" data-aos="zoom-in" data-aos-delay="500">
+      <div class="service-item position-relative">
+        <div class="img">
+          <img src="https://cdn.pixabay.com/photo/2020/03/05/17/36/smart-home-4905026_1280.jpg" class="img-fluid" alt="">
+        </div>
+        <div class="details">
+          <a href="service-details.html" class="stretched-link">
+            <h3>HOME AUTOMATION</h3>
+          </a>
+          <p>
+            Structured cabling installation with Cat6 to Cat8 solutions and fiber optics for high bandwidth.
+          </p>
+        </div>
       </div>
-      <div class="details">
-        <a href="service-details.html" class="stretched-link">
-          <h3>HOME AUTOMATION</h3>
-        </a>
-        <p>
-          • Installation of structured cabling for reliable communication.<br>
-          • Cat6, Cat6A, Cat7, and Cat8 cabling solutions.<br>
-          • Fiber optic cabling for high-bandwidth applications.
-        </p>
-      </div>
-    </div>
-  </div><!-- End Service Item -->
+    </div><!-- End Service Item -->
 
-</div>
+  </div>
 
 </div>
 
